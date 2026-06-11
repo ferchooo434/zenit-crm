2fce02dc51f1d5648e9bab015de728a95de58c65
ferchooo434 <tu@email.com>
2026-06-11 15:03:12 -0600

    descripción del cambio

--- app.py
+++ app.py
@@ -289,30 +289,8 @@ with st.sidebar:
 
     st.markdown("---")
 
-    st.markdown(
-    f"""
-    <div style="
-        padding:10px 0;
-    ">
-        <div style="
-            font-size:1rem;
-            color:#ffffff;
-            font-weight:600;
-        ">
-            👤 {st.session_state.nombre}
-        </div>
-
-        <div style="
-            color:#8a8a8a;
-            font-size:0.80rem;
-            margin-top:2px;
-        ">
-            {st.session_state.rol.title()}
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+    st.write(f"👤 {st.session_state.nombre}")
+    st.caption(st.session_state.rol.title())
     
 # ── DASHBOARD ────────────────────────────────────────────
 if pagina == "Dashboard":
