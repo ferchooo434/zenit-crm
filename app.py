f6a54f49f38a490271c9e59dceaf1f60d091a353
ferchooo434 <tu@email.com>
2026-06-15 12:43:01 -0600

    descripción del cambio

--- app.py
+++ app.py
@@ -283,22 +283,9 @@ with st.sidebar:
         unsafe_allow_html=True
     )
 
-    st.markdown(f"""
-    <div style="padding:0;">
-        <div style="font-size:0.9rem;font-weight:600;color:white;">
-            {st.session_state.nombre}
-        </div>
+    st.write(st.session_state.nombre)
 
-        <div style="
-            font-size:0.7rem;
-            color:#666;
-            margin-top:2px;
-            text-transform:uppercase;
-        ">
-            {st.session_state.rol}
-        </div>
-    </div>
-    """, unsafe_allow_html=True)
+st.caption(st.session_state.rol.upper())
     
 # ── DASHBOARD ────────────────────────────────────────────
 if pagina == "Dashboard":
